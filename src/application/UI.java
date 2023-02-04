a6bcb1955c5ec8ba4746a9221ce5453e6af64057
rafael carvalho <rafael.carv86@gmail.com>
2023-02-04 10:18:41 -0300

    Testando o check

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,6 +57,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Jogada: " + chessMatch.getTurn());
 		System.out.println("Esperando o jogador: " + chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!!");
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,10 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
 	protected boolean isThereOpponentPiece(Position posicion) {
 		ChessPiece p = (ChessPiece)getBoard().piece(posicion);
 		return p != null && p.getColor() != color;
